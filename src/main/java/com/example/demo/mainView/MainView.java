9cc9a477ebb6bc7ea21541971a59344dc6f32efe
gerryS2xda <gerardo96bn@hotmail.it>
2019-12-03 14:03:43 +0100

    MainView: Add support for Responsive

--- src/main/java/com/example/demo/mainView/MainView.java
+++ src/main/java/com/example/demo/mainView/MainView.java
@@ -20,9 +20,11 @@ import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.dependency.StyleSheet;
+import com.vaadin.flow.component.dialog.Dialog;
 import com.vaadin.flow.component.html.*;
 import com.vaadin.flow.component.html.Image;
 import com.vaadin.flow.component.html.Label;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
 import com.vaadin.flow.component.orderedlayout.VerticalLayout;
 import com.vaadin.flow.component.radiobutton.RadioButtonGroup;
 import com.vaadin.flow.component.textfield.PasswordField;
@@ -63,10 +65,19 @@ public class MainView extends VerticalLayout {
     @Autowired
     private GameList gameList;
 
-
+    //instance field
     private TextField email;
     private String confermaReg;
     private Button b;
+    private Image logoImg;
+    private Image pcImg;
+    private Div descrizione; //contiene Label 'description'
+    private VerticalLayout loginContainer;
+    private Div divContainerFormLogin;
+    private VerticalLayout registerContainer;
+    private Div divContainerFormRegister;
+    private HorizontalLayout btnContainer = new HorizontalLayout();
+    private boolean isBtnRegisterLoginShow = false;
 
     public MainView() {
 
@@ -78,53 +89,14 @@ public class MainView extends VerticalLayout {
             }
             else {
             */
-                Image i = new Image("frontend/img/pc.png", "txt");
-                Image img = new Image("frontend/img/logocompleto.png", "txt");
-                //Image image22 = new Image("frontend/img/sigla.png", "txt");
-                //image22.setWidth("200px");
-                //image22.getElement().getStyle().set("position", "absolute");
-                //image22.getElement().getStyle().set("left", "180px");
-                img.setWidth("400px");
-                img.setHeight("120px");
-                img.getStyle().set("top","5px");
-                img.getElement().getStyle().set("position", "absolute");
+                loadMainViewContent();
 
-                i.getElement().getStyle().set("position", "absolute");
-                i.getElement().getStyle().set("width", "43%");
-                i.getElement().getStyle().set("height", "55%");
-                i.getElement().getStyle().set("top", "35%");
-                i.getElement().getStyle().set("opacity", "0.5");
-                i.getElement().getStyle().set("left", "200px");
-                setAlignSelf(Alignment.START, i);
-                setSizeFull();
-                getElement().getStyle().set("background-color", "#b8dbe0");
-                Div descrizione = new Div();
-                Label descr = new Label("\n" +
-                        "ConnecTeam è una piattaforma web che ha come obiettivo l'apprendimento collaborativo tramite dei giochi." +
-                        " I giochi possono essere per qualsiasi fascia d'età e di solito sono prettamente educativi." +
-                        " La collaborazione è fondamentele per ConnecTeam!" +
-                        " Inoltre non mancano i servizi per l'utente, il quale troverà molto semplice gestire il suo profilo!");
-                descr.setWidth("50%");
-                descrizione.setWidth("64%");
-                descrizione.setHeight("10%");
-                descrizione.add(descr);
-                descrizione.getElement().getStyle().set("position", "absolute");
-                descrizione.getElement().getStyle().set("top", "150px");
-                login();
-                register();
-                add(/*image22,*/descrizione,img,i);
-
-                //Button che mostra un lista di utenti gia' registrati (Testing)
-                Button button = new Button();
-                button.addClickListener(buttonClickEvent -> {
-                    buttons();
+                //Using Browser Window Resize Events for responsive
+                UI.getCurrent().getPage().addBrowserWindowResizeListener(browserWindowResizeEvent -> {
+                    System.out.println("MainView- Responsive  width: " + browserWindowResizeEvent.getWidth() + " height:" + browserWindowResizeEvent.getHeight());
+                    loadResponsiveConfiguration(browserWindowResizeEvent.getWidth(), browserWindowResizeEvent.getHeight());
                 });
-                add(button);
 
-                /*UI.getCurrent().getPage().addBrowserWindowResizeListener(browserWindowResizeEvent -> {
-                    Notification.show("Window width=" + browserWindowResizeEvent.getWidth()
-                            + ", height=" + browserWindowResizeEvent.getHeight());
-                });*/
         }catch (Exception e){
             removeAll();
             ErrorPage errorPage = new ErrorPage();
@@ -133,9 +105,75 @@ public class MainView extends VerticalLayout {
         }
     }
 
+    private void loadMainViewContent(){
 
-    private void login(){
+        isBtnRegisterLoginShow = false;
 
+        setSizeFull();
+        getElement().getStyle().set("background-color", "#b8dbe0");
+
+        logoImg = logoConnectTeam();
+        pcImg = pcImage();
+
+        loginContainer = login();
+        add(loginContainer);
+
+        registerContainer = register();
+        add(registerContainer);
+
+        descrizione = descrizioneContainer();
+        add(descrizione, logoImg, pcImg);
+
+        //Button che mostra un lista di utenti gia' registrati (Testing)
+        Button button = new Button();
+        button.addClickListener(buttonClickEvent -> {
+            buttons();
+        });
+        add(button);
+    }
+
+    private Image logoConnectTeam(){
+        Image img = new Image("frontend/img/logocompleto.png", "txt");
+        img.setWidth("400px");
+        img.setHeight("120px");
+        img.getStyle().set("top","5px");
+        img.getElement().getStyle().set("position", "absolute");
+        return img;
+    }
+
+    private Image pcImage(){
+        Image img = new Image("frontend/img/pc.png", "txt");
+        img.getElement().getStyle().set("position", "absolute");
+        img.getElement().getStyle().set("width", "43%");
+        img.getElement().getStyle().set("height", "55%");
+        img.getElement().getStyle().set("top", "230px"); //valore precedente: 35%
+        img.getElement().getStyle().set("opacity", "0.5");
+        img.getElement().getStyle().set("left", "200px");
+        setAlignSelf(Alignment.START, img);
+        return img;
+    }
+
+    private Div descrizioneContainer(){
+        Div container = new Div();
+        Label descr = new Label("\n" +
+                "ConnecTeam è una piattaforma web che ha come obiettivo l'apprendimento collaborativo tramite dei giochi." +
+                " I giochi possono essere per qualsiasi fascia d'età e di solito sono prettamente educativi." +
+                " La collaborazione è fondamentale per ConnecTeam!" +
+                " Inoltre non mancano i servizi per l'utente, il quale troverà molto semplice gestire il suo profilo!");
+        descr.setWidth("100%");
+        container.setWidth("61%"); //valore precedente: 64%
+        container.setHeight("10%");
+        container.getElement().getStyle().set("position", "absolute");
+        container.getElement().getStyle().set("top", "150px");
+        container.add(descr);
+        return container;
+    }
+
+    private VerticalLayout login(){
+
+        VerticalLayout divFormLogin = new VerticalLayout();
+
+        //content
         TextField emailField = new TextField("Email");
         PasswordField passwordField = new PasswordField("Password");
         passwordField.getElement().getStyle().set("margin-left","10px");
@@ -186,7 +224,6 @@ public class MainView extends VerticalLayout {
 
         Div space = new Div();
         space.setWidth("100%");
-        VerticalLayout divForm = new VerticalLayout();
 
         Span s = new Span();
         s.getStyle().set("background-color", "#007d99");
@@ -195,15 +232,18 @@ public class MainView extends VerticalLayout {
         s.getStyle().set("width", "90%");
         s.getStyle().set("margin-top", "10px");
 
-        Div divContainerForm = new Div();
-        divContainerForm.add(emailField,passwordField,login,space,passwordDimenticata);
-        divForm.setHorizontalComponentAlignment(Alignment.END,divContainerForm);
-        divForm.add(divContainerForm,s);
-        add(divForm);
+        divContainerFormLogin = new Div();
+        divContainerFormLogin.add(emailField,passwordField,login,space,passwordDimenticata);
+        divFormLogin.setHorizontalComponentAlignment(Alignment.END,divContainerFormLogin);
+        divFormLogin.add(divContainerFormLogin,s);
+        return divFormLogin;
     }
 
-    private void register(){
+    private VerticalLayout register(){
 
+        VerticalLayout divForm = new VerticalLayout();
+
+        //Content
         Label label = new Label("Crea un nuovo account");
         label.getElement().getStyle().set("font-size","30px");
 
@@ -260,9 +300,7 @@ public class MainView extends VerticalLayout {
         binder.addStatusChangeListener(
                 event -> submit.setEnabled(binder.isValid()));
 
-        VerticalLayout divForm = new VerticalLayout();
-
-        Div divContainerForm = new Div();
+        divContainerFormRegister = new Div();
 
         submit.addClickListener(buttonClickEvent -> {
             if (group.getValue()=="Uomo"){
@@ -278,9 +316,9 @@ public class MainView extends VerticalLayout {
             registraNewAccount(binder.getBean());
         });
 
-        divContainerForm.getElement().getStyle().set("margin-right","100px");
-        divContainerForm.getElement().getStyle().set("position","relative");
-        divContainerForm.getElement().getStyle().set("top","-20px");
+        divContainerFormRegister.getElement().getStyle().set("margin-right","100px");
+        divContainerFormRegister.getElement().getStyle().set("position","relative");
+        divContainerFormRegister.getElement().getStyle().set("top","-20px");
 
         Div space = new Div();
         space.setWidth("100%");
@@ -289,11 +327,11 @@ public class MainView extends VerticalLayout {
         Div space3 = new Div();
         space3.setWidth("100%");
 
-        divContainerForm.add(label,space3,nome,email,space,password,passwordC,space2,group,submit);
-        divForm.setHorizontalComponentAlignment(Alignment.END,divContainerForm);
+        divContainerFormRegister.add(label,space3,nome,email,space,password,passwordC,space2,group,submit);
+        divForm.setHorizontalComponentAlignment(Alignment.END,divContainerFormRegister);
 
-        divForm.add(divContainerForm);
-        add(divForm);
+        divForm.add(divContainerFormRegister);
+        return divForm;
     }
 
     private void registraNewAccount(Account account){
@@ -323,6 +361,78 @@ public class MainView extends VerticalLayout {
         }
         DialogUtility dialogUtility = new DialogUtility();
         dialogUtility.loginDialog(account);
+    }
+
+    private void loadResponsiveConfiguration(int widthBrowser, int heightBrowser){
+
+        if(widthBrowser < 1000 && heightBrowser <= 500){
+            pcImg.getStyle().set("display", "none");
+            descrizione.getStyle().set("margin-left", "20px");
+            descrizione.getStyle().set("width", "35%");
+            descrizione.getStyle().set("top", "180px");
+            divContainerFormRegister.getStyle().set("margin-right", "0");
+            divContainerFormRegister.getStyle().set("position", "relative");
+            divContainerFormRegister.getStyle().set("right", "10%");
+        }else{
+            removeAll();
+            loadMainViewContent();
+            return;
+        }
+
+        if(widthBrowser <= 850 && heightBrowser <= 500){
+            logoImg.setWidth("40%");
+            logoImg.setHeight("25%");
+            divContainerFormRegister.getStyle().set("right", "0");
+        }else {
+            divContainerFormRegister.getStyle().set("right", "10%");
+        }
+
+        if(widthBrowser <= 770 && heightBrowser <= 500){
+            descrizione.getStyle().set("width", "85%");
+            if(!isBtnRegisterLoginShow) {
+               divContainerFormLogin.getStyle().set("display", "none");
+               registerContainer.getStyle().set("display", "none");
+               loginContainer.getStyle().set("position", "relative");
+               loginContainer.getStyle().set("top", "120px");
+               btnContainer = buttonLoginRegisterContainer();
+               add(btnContainer);
+               isBtnRegisterLoginShow = true;
+           }
+        }else{
+            descrizione.getStyle().set("width", "35%");
+            divContainerFormLogin.getStyle().set("display", "block");
+            registerContainer.getStyle().set("display", "flex"); //VerticalLayout richiede display:flex
+            loginContainer.getStyle().set("position", "relative");
+            loginContainer.getStyle().set("top", "0");
+            remove(btnContainer);
+        }
+    }
+
+    private HorizontalLayout buttonLoginRegisterContainer(){
+        HorizontalLayout container = new HorizontalLayout();
+        container.getStyle().set("position", "absolute");
+        container.getStyle().set("left", "50%");
+
+        Button login = new Button("Login");
+        login.addClickListener(buttonClickEvent -> {
+           Dialog d = new Dialog();
+           d.setWidth("100%");
+           d.setHeight("100%");
+           d.add(login());
+           d.open();
+        });
+
+        Button register = new Button("Registrazione");
+        register.addClickListener(buttonClickEvent -> {
+            Dialog d = new Dialog();
+            d.setWidth("100%");
+            d.setHeight("100%");
+            d.add(register());
+            d.open();
+        });
+
+        container.add(login, register);
+        return container;
     }
 
     //Test buttons per accedere direttamente al sito con account gia' registrati
