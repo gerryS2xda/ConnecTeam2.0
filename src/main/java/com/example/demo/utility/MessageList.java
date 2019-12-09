ca078b4c1c4117adaaded88e8813a380e166a5c8
gerryS2xda <gerardo96bn@hotmail.it>
2019-12-09 21:01:46 +0100

    No desc

--- src/main/java/com/example/demo/utility/MessageList.java
+++ src/main/java/com/example/demo/utility/MessageList.java
@@ -5,15 +5,15 @@ import com.vaadin.flow.component.html.Div;
 
 public class MessageList extends Div {
 
-  public MessageList(String className) {
-    addClassName(className);
-  }
+    //Constructors
+    public MessageList(String className) {
+      addClassName(className);
+    }
 
-  @Override
-  public void add(Component... components) {
-    super.add(components);
-    components[components.length-1].getElement().callFunction("scrollIntoView");
-  }
-
+    @Override
+    public void add(Component... components) {
+      super.add(components);
+      components[components.length - 1].getElement().callFunction("scrollIntoView");
+    }
 }
 
