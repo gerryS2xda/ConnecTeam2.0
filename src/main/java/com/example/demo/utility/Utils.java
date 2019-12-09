003ff3fa1602d50a1fc4111651684092059086f1
gerryS2xda <gerardo96bn@hotmail.it>
2019-12-09 12:06:33 +0100

    Utils: add getComponentByElementId() method

--- src/main/java/com/example/demo/utility/Utils.java
+++ src/main/java/com/example/demo/utility/Utils.java
@@ -2,8 +2,10 @@ package com.example.demo.utility;
 
 import com.example.demo.entity.Account;
 import com.example.demo.entity.Gruppo;
+import com.vaadin.flow.component.Component;
 
 import java.util.List;
+import java.util.Map;
 
 public class Utils {
 
@@ -44,5 +46,18 @@ public class Utils {
             }
         }
         return x;
+    }
+
+    public static Component getComponentByElementId(Map<Account, Component> azioni, String valueId){
+        Component c = null;
+
+        for(Account i : azioni.keySet()){
+            String str = azioni.get(i).getElement().getAttribute("id");
+            if(str.equals(valueId)){
+                c = azioni.get(i);
+                break;
+            }
+        }
+        return c;
     }
 }
