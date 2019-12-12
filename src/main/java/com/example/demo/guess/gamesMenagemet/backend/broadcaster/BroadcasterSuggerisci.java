b2b8ac2de841ceb194cd1076dbd0abd2adf2f965
gerryS2xda <Qwerty96xda>
2019-12-12 17:23:58 +0100

    BroadcastSuggerisci: replace List<SuggerisciListener> with Map<Account, SuggerisciListener> per mappare un listener ad ogni account

--- src/main/java/com/example/demo/guess/gamesMenagemet/backend/broadcaster/BroadcasterSuggerisci.java
+++ src/main/java/com/example/demo/guess/gamesMenagemet/backend/broadcaster/BroadcasterSuggerisci.java
@@ -1,28 +1,28 @@
 package com.example.demo.guess.gamesMenagemet.backend.broadcaster;
 
+import com.example.demo.entity.Account;
 import com.example.demo.entity.Gruppo;
 import com.example.demo.guess.gamesMenagemet.backend.listeners.SuggerisciListener;
-
 import java.io.Serializable;
-import java.util.List;
-import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class BroadcasterSuggerisci implements Serializable {
 
-    private static final List<SuggerisciListener> listeners = new CopyOnWriteArrayList<SuggerisciListener>();
+    private static final Map<Account, SuggerisciListener> listeners = new HashMap<>();
 
-    public static synchronized void register(SuggerisciListener listener) {
-        listeners.add(listener);
+    public static synchronized void register(Account account, SuggerisciListener listener) {
+        listeners.put(account, listener);
     }
 
     public static void broadcast(Gruppo g, final String message) {
-        for (SuggerisciListener listener : listeners) {
-            listener.receiveBroadcast(g, message);
-        }
+
+        listeners.forEach((account, broadcastListener) -> {
+            broadcastListener.receiveBroadcast(g, message);
+        });
     }
 
-    public static List<SuggerisciListener> getListeners() {
+    public static Map<Account, SuggerisciListener> getListeners() {
         return listeners;
     }
-
 }
