6f4c9ad23d517fb2d90330dab579ada9174f4e61
gerryS2xda <Qwerty96xda>
2019-12-14 11:03:50 +0100

    ChatListener: add 'Gruppo g' argument in receiveBroadcast() per indicare il gruppo da cui e' stato inviato un messaggio

--- src/main/java/com/example/demo/guess/gamesMenagemet/backend/listeners/ChatListener.java
+++ src/main/java/com/example/demo/guess/gamesMenagemet/backend/listeners/ChatListener.java
@@ -1,5 +1,7 @@
 package com.example.demo.guess.gamesMenagemet.backend.listeners;
 
+import com.example.demo.entity.Gruppo;
+
 public interface ChatListener {
-    void receiveBroadcast(String message);
+    void receiveBroadcast(Gruppo g, String message); //g e' il gruppo da cui e' stato inviato il messaggio
 }
