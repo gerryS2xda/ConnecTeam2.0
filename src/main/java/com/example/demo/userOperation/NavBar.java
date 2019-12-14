51d62dc655752beb3210c1489bc4aebf6cb10a04
gerryS2xda <Qwerty96xda>
2019-12-14 20:01:21 +0100

    NavBar: sistemazione codice, add 'isGamePage' argument in constructor; add business logic per distinguere la navBar tra quella da mostrare nei giochi e quella da mostrare nelle altre pagine

--- src/main/java/com/example/demo/userOperation/NavBar.java
+++ src/main/java/com/example/demo/userOperation/NavBar.java
@@ -2,12 +2,12 @@ package com.example.demo.userOperation;
 
 import com.example.demo.mainView.MainView;
 import com.example.demo.users.discusser.StudentHomeView;
+import com.example.demo.utility.DialogUtility;
 import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.dependency.HtmlImport;
 import com.vaadin.flow.component.dependency.StyleSheet;
 import com.vaadin.flow.component.html.Div;
-import com.vaadin.flow.component.html.UnorderedList;
 import com.vaadin.flow.component.icon.Icon;
 import com.vaadin.flow.component.icon.VaadinIcon;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -24,84 +24,29 @@ import java.nio.charset.StandardCharsets;
 public class NavBar extends HorizontalLayout {
 
     //instance field
+    private HorizontalLayout main;
     private HorizontalLayout homeContainer;
+    private HorizontalLayout homeContainerWithBtn;
     private HorizontalLayout settingsContainer;
     private HorizontalLayout statisticsContainer;
+    private HorizontalLayout chatContainer; //verra' settato in GuessUI/MatyUI
     private HorizontalLayout logOutContainer;
+    private boolean isGamePage;
 
     //Questa barra di navigazione e' stata usata solo per i discusser
-    public NavBar (){
+    public NavBar (boolean isGamePage){
+
+        this.isGamePage = isGamePage;
 
         //Style nav bar orizzontale
         getStyle().set("background-color","#84c0c9");
         getStyle().set("padding","0px");
         getElement().getStyle().set("width", "100%");
         getStyle().set("border-radius","10%");
-        UnorderedList unorderedList = new UnorderedList();
-        HorizontalLayout main = new HorizontalLayout();
+
+        main = new HorizontalLayout();
         main.getStyle().set("padding","10px");
 
-        //Container per pulsante "Home" e link collegamento
-        homeContainer = new HorizontalLayout();
-        Icon homeIcon = new Icon(VaadinIcon.HOME);
-        homeIcon.setSize("30px");
-        homeIcon.setColor("#007d99");
-        RouterLink routerLink = new RouterLink("Home", StudentHomeView.class);
-        routerLink.getStyle().set("text-decoration", "none");
-        routerLink.addClassName("router-link");
-        unorderedList.add(routerLink);
-        Div space = new Div();
-        space.setWidth("100%");
-        homeContainer.add(homeIcon,routerLink);
-
-        //Container per pulsante "Impostazioni" e link collegamento
-        settingsContainer = new HorizontalLayout();
-        Icon settingIcon = new Icon(VaadinIcon.COGS);
-        settingIcon.setSize("30px");
-        settingIcon.setColor("#007d99");
-        RouterLink routerLink1 = new RouterLink("Impostazioni", SettingsUser.class);
-        routerLink1.getStyle().set("text-decoration", "none");
-        routerLink1.addClassName("router-link");
-        Div space1 = new Div();
-        space1.setWidth("100%");
-        settingsContainer.add(settingIcon,routerLink1);
-
-        //Container per pulsante "Statistiche" e link collegamento
-        statisticsContainer = new HorizontalLayout();
-        Icon statisticheIcon = new Icon(VaadinIcon.PIE_BAR_CHART);
-        statisticheIcon.setSize("30px");
-        statisticheIcon.setSize("30px");
-        statisticheIcon.setColor("#007d99");
-        RouterLink routerLink2 = new RouterLink("Statistiche", StatisticUser.class);
-        routerLink2.getStyle().set("text-decoration", "none");
-        routerLink2.addClassName("router-link");  //style extra definito in stile.css
-        Div space2 = new Div();
-        space2.setWidth("100%");
-        statisticsContainer.add(statisticheIcon,routerLink2);
-
-        //Container per pulsante "Logout" e link collegamento
-        logOutContainer = new HorizontalLayout();
-        logOutContainer.getStyle().set("position","absolute");
-        logOutContainer.getStyle().set("left","88%");
-        logOutContainer.getStyle().set("top", "6px");
-
-        Icon logoutIcon = new Icon(VaadinIcon.SIGN_OUT);
-        logoutIcon.setSize("30px");
-        logoutIcon.setColor("#007d99");
-        logoutIcon.getStyle().set("margin-top", "6px");
-        logoutIcon.getStyle().set("margin-right", "8px");
-
-        Button routerLink3 = new Button("Logout");
-        routerLink3.addClickListener(buttonClickEvent -> {
-            VaadinSession.getCurrent().getSession().invalidate();
-            UI.getCurrent().navigate(MainView.class);  //vai alla pagina "MainView" (classe con @Route("MainView")
-            System.out.println(VaadinService.getCurrentRequest().getWrappedSession().getAttribute("user"));
-        });
-
-        routerLink3.addClassName("buttonLogOut");
-        Div space3 = new Div();
-        space3.setWidth("100%");
-        logOutContainer.add(logoutIcon,routerLink3);
         String styles = ".router-link { "
                 + "margin-left: 10px;"
                 + "font-size: 20px;"
@@ -116,7 +61,13 @@ public class NavBar extends HorizontalLayout {
         UI.getCurrent().getPage().addStyleSheet(
                 "base://" + resource.getResourceUri().toString());
 
-        main.add(homeContainer, settingsContainer, statisticsContainer, logOutContainer);  //aggingi i div alla nav bar
+        if(isGamePage){
+            chatContainer = new HorizontalLayout();
+            main.add(homeContainerWithBtn(), chatContainer, logOutContainer());
+        }else{
+            main.add(homeContainer(), settingsContainer(), statisticsContainer(), logOutContainer());  //aggingi i div alla nav bar
+        }
+
         add(main);  //aggiungi la navbar nella view generale
 
         //Using Browser Window Resize Events for responsive
@@ -125,6 +76,105 @@ public class NavBar extends HorizontalLayout {
         });
     }
 
+    private HorizontalLayout homeContainer(){
+        //Container per pulsante "Home" e link collegamento
+        homeContainer = new HorizontalLayout();
+        Icon homeIcon = new Icon(VaadinIcon.HOME);
+        homeIcon.setSize("30px");
+        homeIcon.setColor("#007d99");
+        RouterLink routerLink = new RouterLink("Home", StudentHomeView.class);
+        routerLink.getStyle().set("text-decoration", "none");
+        routerLink.addClassName("router-link");
+        homeContainer.add(homeIcon,routerLink);
+        return homeContainer;
+    }
+
+    private HorizontalLayout homeContainerWithBtn(){
+        //Container per pulsante "Home" (usato per Game)
+        homeContainerWithBtn = new HorizontalLayout();
+        Icon homeIcon = new Icon(VaadinIcon.HOME);
+        homeIcon.setSize("30px");
+        homeIcon.setColor("#007d99");
+
+        //NOTA: Quando la partita termina, viene rimosso tutto il contenuto della pagina e quindi la navbar non viene mostrata
+        Button routerLink3 = new Button("Home");
+        routerLink3.addClickListener(buttonClickEvent -> {
+            DialogUtility dialogUtility = new DialogUtility();
+            dialogUtility.showConfirmDialogForGame("Partita in corso", "Sei sicuro di voler abbandonare la partita in corso?", false).open();
+        });
+
+        routerLink3.addClassName("buttonLogOut");
+        homeContainerWithBtn.add(homeIcon,routerLink3);
+        return homeContainerWithBtn;
+    }
+
+    private HorizontalLayout settingsContainer(){
+        //Container per pulsante "Impostazioni" e link collegamento
+        settingsContainer = new HorizontalLayout();
+        Icon settingIcon = new Icon(VaadinIcon.COGS);
+        settingIcon.setSize("30px");
+        settingIcon.setColor("#007d99");
+        RouterLink routerLink1 = new RouterLink("Impostazioni", SettingsUser.class);
+        routerLink1.getStyle().set("text-decoration", "none");
+        routerLink1.addClassName("router-link");
+        settingsContainer.add(settingIcon,routerLink1);
+        return settingsContainer;
+    }
+
+    private HorizontalLayout statisticsContainer(){
+        //Container per pulsante "Statistiche" e link collegamento
+        statisticsContainer = new HorizontalLayout();
+        Icon statisticheIcon = new Icon(VaadinIcon.PIE_BAR_CHART);
+        statisticheIcon.setSize("30px");
+        statisticheIcon.setSize("30px");
+        statisticheIcon.setColor("#007d99");
+        RouterLink routerLink2 = new RouterLink("Statistiche", StatisticUser.class);
+        routerLink2.getStyle().set("text-decoration", "none");
+        routerLink2.addClassName("router-link");  //style extra definito in stile.css
+        statisticsContainer.add(statisticheIcon,routerLink2);
+        return statisticsContainer;
+    }
+
+    private HorizontalLayout logOutContainer(){
+        //Container per pulsante "Logout" e link collegamento
+        logOutContainer = new HorizontalLayout();
+        logOutContainer.getStyle().set("position","absolute");
+        logOutContainer.getStyle().set("left","88%");
+        logOutContainer.getStyle().set("top", "6px");
+
+        Icon logoutIcon = new Icon(VaadinIcon.SIGN_OUT);
+        logoutIcon.setSize("30px");
+        logoutIcon.setColor("#007d99");
+        logoutIcon.getStyle().set("margin-top", "6px");
+        logoutIcon.getStyle().set("margin-right", "8px");
+
+        Button routerLink3 = new Button("Logout");
+        routerLink3.addClickListener(buttonClickEvent -> {
+            if(isGamePage){
+                DialogUtility dialogUtility = new DialogUtility();
+                dialogUtility.showConfirmDialogForGame("Partita in corso", "Sei sicuro di voler abbandonare la partita in corso?", true).open();
+            }else {
+                VaadinSession.getCurrent().getSession().invalidate();
+                UI.getCurrent().navigate(MainView.class);  //vai alla pagina "MainView" (classe con @Route("MainView")
+                System.out.println(VaadinService.getCurrentRequest().getWrappedSession().getAttribute("user"));
+            }
+        });
+
+        routerLink3.addClassName("buttonLogOut");
+        logOutContainer.add(logoutIcon,routerLink3);
+        return logOutContainer;
+    }
+
+    //Getter and Setter
+    public HorizontalLayout getChatContainer() {
+        return chatContainer;
+    }
+
+    public void setChatContainer(HorizontalLayout chatContainer) {
+        this.chatContainer = chatContainer;
+    }
+
+    //Responsive methods
     private void loadResponsiveConfiguration(int widthBrowser, int heightBrowser){
 
         if(widthBrowser <= 1000){
